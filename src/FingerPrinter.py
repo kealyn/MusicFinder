90b1318d8e8b1f5fda3aef220e0d59ebe943d85d
Lin Qi <kealyn1002@gmail.com>
2018-02-21 15:19:25 -0500

    Formatted indentations and added one comment

--- src/FingerPrinter.py
+++ src/FingerPrinter.py
@@ -6,7 +6,7 @@ Class that is responsible for computing finger prints from the channel samples
 '''
 class FingerPrinter:
 
-	'''
+    '''
     Constructor
     '''
     def __init__(self):
@@ -17,13 +17,13 @@ class FingerPrinter:
 	locally sensitive hashes.
 	'''
 	def fingerprint(self, channel_samples, fs):
+	    # ToDo: To be completed
+            # Step 1: FFT the signal and extract frequency components
+	    spectrum = self.FFT.run(channel_samples, fs)
 		
-		# Step 1: FFT the signal and extract frequency components
-		spectrum = self.FFT.run(channel_samples, fs)
-
-		# Step 2: Find peak points
+	    # Step 2: Find peak points
 		
-		# Step 3: Compute hashes
+	    # Step 3: Compute hashes
 		
-		# return hashes
+	    # return hashes
 
